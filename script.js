11ed9030deb9486732ff80c8c3c698b8dda5ded4
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-05-26 13:55:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  const secondHand = document.querySelector('.second-hand');
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+  const minuteHand = document.querySelector('.minute-hand');
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+  const hourHand = document.querySelector('.hour-hand');
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate(); // Initial call to set the time immediately when the page loads
